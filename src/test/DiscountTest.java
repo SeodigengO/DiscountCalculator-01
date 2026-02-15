9e7029cbdd6f891fd24c48cfb739d14700725599
OSeodigeng <seodigengo@gmail.com>
2026-02-16 01:16:19 +0200

    Added tests to test the discount Price and discounted Amount

--- src/test/DiscountTest.java
+++ src/test/DiscountTest.java
@@ -1,1 +1,22 @@
+package test;
 
+import main.DiscountCalculator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DiscountTest {
+
+    @Test
+    void shouldCalculateDiscountAmount() {
+        DiscountCalculator calculator =  new DiscountCalculator(100.0, 10.0);
+        assertEquals(10.0, calculator.discountPrice(), 0.001);
+    }
+
+    @Test
+    void shouldCalculateDiscountedPrice() {
+        DiscountCalculator calculator =  new DiscountCalculator(100.0, 10.0);
+        assertEquals(90.0, calculator.discountedPrice(), 0.001);
+    }
+
+}
